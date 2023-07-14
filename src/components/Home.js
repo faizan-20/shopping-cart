bc71243573707a7cb529a250246bb27bd274c614
faizan-20 <faizan.20a@gmail.com>
2023-07-14 14:05:53 +0530

    finished home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div id="home" className="flex flex-col items-center">
-            <div className="head text-5xl ">Home Store</div>
-            <div className="text">State of the art shop</div>
-            <Link to="/shop" className=" rounded-md border-lime-200 border-2 px-2 py-1 bg-lime-100">Start Shopping </Link>
+        <div id="home" 
+            className="flex flex-col items-center"
+        >
+            <div className="head text-6xl font-bold text-white mt-10">For All Your Bread Needs</div>
+            <div className="text text-white mt-4 text-xl">Get breads from round the world</div>
+            <Link to="/shop" className=" rounded-md border-white-200 border-2 px-2 py-1 mt-10 text-slate-200 font-bold hover:bg-slate-200 hover:text-slate-900">Shop Now</Link>
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
     return (
-        <nav className="flex justify-around items-center px-2 py-2 bg-cyan-800 text-slate-300 font-bold">
-            <div className="links flex">
-                <Link to="/" className="hover:text-white">Home</Link>
-                <Link to="/shop" className="ml-4 hover:text-white">Shop</Link>
-                <div className="cart ml-4">Cart</div>
-            </div>
+        <nav className="flex justify-around items-center px-2 py-2 bg-black text-slate-300 font-bold sticky top-3">
             <div className="head text-slate-200 text-2xl">
-                Some Heading
+                Bread:Crumbs
+            </div>
+            <div className="links flex">
+                <Link to="/" className="rounded-md px-1 py-1 text-slate-200 font-bold hover:border-slate-200 border-2 border-black">Home</Link>
+                <Link to="/shop" className="rounded-md px-1 py-1 text-slate-200 font-bold hover:border-slate-200 border-2 border-black">Shop</Link>
+                <div className="rounded-md px-1 py-1 text-slate-200 font-bold hover:border-slate-200 border-2 border-black hover:cursor-pointer">Cart</div>
             </div>
         </nav>
     )
